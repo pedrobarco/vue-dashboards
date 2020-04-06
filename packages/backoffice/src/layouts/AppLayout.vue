2d8d0e5b18b777d780f9f16219da2e0b9c4a7cad
Pedro Barco <pedro.barco@linkconsulting.com>
2020-04-06 19:56:24 +0100

    feat: add sidebar entries for dashboard, users, sales and clients

--- packages/backoffice/src/layouts/AppLayout.vue
+++ packages/backoffice/src/layouts/AppLayout.vue
@@ -1,14 +1,42 @@
 <template>
-  <div>
-    <h1 class="text-5xl font-bold text-center text-gray-700">App Layout</h1>
-    <div class="container">
-      <router-view />
+  <div class="flex min-h-screen">
+    <div class="w-56 bg-gray-100 shadow border-r">
+      <img class="h-12 mx-auto mt-8" src="/img/logo.svg" alt="Lintifor" />
+      <div class="flex flex-col mt-8">
+        <router-link
+          class="flex items-center mb-4 px-2 py-4 rounded-sm font-medium text-gray-700 hover:text-primary-600 hover:bg-gray-200"
+          active-class="text-primary-600 bg-gray-200 border-r-4 border-primary-500 route-icon-active"
+          v-for="route in appRoutes"
+          v-bind:key="route.name"
+          :to="route.path"
+        >
+          <div
+            v-html="route.metadata.icon"
+            class="h-6 w-6 route-icon ml-4 mr-6"
+          />
+          {{ route.name }}
+        </router-link>
+      </div>
     </div>
+    <div class="bg-gray-200 flex-1"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-property-decorator";
+import { Vue, Component } from "vue-property-decorator";
+import { appRoutes } from "@/plugins/router";
 
-export default class Private extends Vue {}
+@Component
+export default class AppLayout extends Vue {
+  appRoutes = appRoutes;
+}
 </script>
+
+<style lang="postcss">
+.route-icon > svg {
+  @apply text-primary-400;
+}
+.route-icon-active > div > svg {
+  @apply text-primary-500;
+}
+</style>
--- packages/backoffice/src/plugins/router/index.ts
+++ packages/backoffice/src/plugins/router/index.ts
@@ -1,30 +1,85 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import VueRouter, { NavigationGuard } from "vue-router";
 import { Guards } from "./router.guards";
+import { env } from "@/plugins/env";
 
 Vue.use(VueRouter);
+
+function routerGuard(guard: NavigationGuard) {
+  if (!env.node.isDevelopment) {
+    return guard;
+  }
+}
+
+export const appRoutes = [
+  {
+    path: "dashboard",
+    alias: "",
+    name: "Dashboard",
+    metadata: {
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pie-chart">
+              <path d="M21.21 15.89A10 10 0 1 1 8 2.83"/>
+              <path d="M22 12A10 10 0 0 0 12 2v10z"/>
+            </svg>`
+    },
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "@/views/Dashboard.vue")
+  },
+  {
+    path: "sales",
+    name: "Sales",
+    metadata: {
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag">
+              <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/>
+              <line x1="3" y1="6" x2="21" y2="6"/>
+              <path d="M16 10a4 4 0 0 1-8 0"/>
+            </svg>`
+    },
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "@/views/Dashboard.vue")
+  },
+  {
+    path: "products",
+    name: "Products",
+    metadata: {
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box">
+              <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/>
+              <polyline points="3.27 6.96 12 12.01 20.73 6.96"/>
+              <line x1="12" y1="22.08" x2="12" y2="12"/>
+            </svg>`
+    },
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "@/views/Dashboard.vue")
+  },
+  {
+    path: "clients",
+    name: "Clients",
+    metadata: {
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
+              <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+              <circle cx="9" cy="7" r="4"/>
+              <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
+              <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+            </svg>`
+    },
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "@/views/Dashboard.vue")
+  }
+];
 
 const routes = [
   {
     path: "/login",
     name: "login",
-    beforeEnter: Guards.redirectHome,
+    beforeEnter: routerGuard(Guards.redirectHome),
     component: () => import(/* webpackChunkName: "login" */ "@/views/Login.vue")
   },
   {
     path: "/",
     component: () =>
       import(/* webpackChunkName: "appLayout" */ "@/layouts/AppLayout.vue"),
-    beforeEnter: Guards.requireAuth,
-    children: [
-      {
-        path: "dashboard",
-        alias: "",
-        name: "Dashboard",
-        component: () =>
-          import(/* webpackChunkName: "dashboard" */ "@/views/Dashboard.vue")
-      }
-    ]
+    beforeEnter: routerGuard(Guards.requireAuth),
+    children: appRoutes
   },
   {
     path: "*",
